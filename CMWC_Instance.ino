6b757caaa2878c4fea96205fc99c85403f14c492
Lauren Wrubleski <lauren@pop-os.localdomain>
2020-06-23 14:48:51 -0600

    Add basic template for CMWC

---
+++ CMWC_Instance.ino
@@ -0,0 +1,6 @@
+class CMWC_Instance {
+  private:
+  int sensor_power_n;
+  int valve_power;
+  int
+}
---
+++ Coffee_Maker_Water_Controller.ino
@@ -0,0 +1,16 @@
+#define DEBUG_MODE
+
+#include <TaskScheduler.h>
+#include "CMWC_Instance.h"
+
+Scheduler ts;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
